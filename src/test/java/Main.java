a89fdf4ff6eb1adbec6a210697292ef0b6bd2d8b
Darkness <darkness.itachi@gmail.com>
2016-09-29 13:44:29 +0800

    dd

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -10,11 +10,11 @@ public class Main {
 	}
 	
 	public static void convert() {
-		String[] files = { "1.dps", "2.et", "3.wps", "4.ppt", "5.xlsx", "6.pptx", "7.vsd", "8.doc", "9.docx" };
+		String[] files = { "1.dps", "2.et", "3.wps", "4.ppt", "5.xlsx", "6.pptx", "7.vsd", "8.doc", "9.docx", "10.txt" };
 
 		String userDir = System.getProperty("user.dir") + File.separator;
 		String testFileFolderPath = userDir + "test" + File.separator;
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < 10; i++) {
 			File inputFile = new File(testFileFolderPath + files[i]);
 			File outputFile = new File(testFileFolderPath + (i + 1) + ".pdf");
 			new Thread(new ConverThread(inputFile, outputFile)).start();
